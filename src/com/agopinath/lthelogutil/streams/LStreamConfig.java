adadf4a84b9dbd31c35761915ff66d3ff31740c2
Ajay Gopinath <ajgopi124@gmail.com>
2012-09-08 13:58:04 -0700

    fixed licenses

--- src/com/agopinath/lthelogutil/streams/LStreamConfig.java
+++ src/com/agopinath/lthelogutil/streams/LStreamConfig.java
@@ -1,3 +1,21 @@
+/**  
+ * L - the light-weight Java logging utility designed for brevity and simplicity.
+ * Copyright (C) 2012 Ajay Gopinath
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * 
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package com.agopinath.lthelogutil.streams;
 
 public final class LStreamConfig {

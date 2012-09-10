48020d3d8c19bee1e7a59b4d39c49fcfdb35d392
Ajay Gopinath <ajgopi124@gmail.com>
2012-09-09 23:33:18 -0700

    creating LGuiStream for graphical debugging purposes

---
+++ src/com/agopinath/lthelogutil/streams/LGuiStream.java
@@ -0,0 +1,26 @@
+package com.agopinath.lthelogutil.streams;
+
+public class LGuiStream extends LStream {
+
+	@Override
+	public void streamOpen() {
+
+	}
+
+	@Override
+	public void streamClose() {
+
+	}
+
+	@Override
+	public boolean streamIsWritable() {
+
+		return false;
+	}
+
+	@Override
+	public String streamWrite(String output) {
+
+		return null;
+	}
+}
